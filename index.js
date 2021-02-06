d79b78bda04cbdf435e42535d45c155628dda3bc
ermurray <ermurray31@gmail.com>
2021-02-06 18:16:34 +0000

    refactor to use single function

--- index.js
+++ index.js
@@ -1,28 +1,40 @@
 // eslint-disable-next-line func-style
-function sumItems(array) {
-  let flatArr = flattenArray(array);
-  let sum = sumFlatArray(flatArr);
-  return sum;
-}
+// function sumItems(array) {
+//   let flatArr = flattenArray(array);
+//   let sum = sumFlatArray(flatArr);
+//   return sum;
+// }
+// // eslint-disable-next-line func-style
+// function flattenArray(array) {
+//   let flatArray = [];
+//   array.forEach(item => {
+//     if (Array.isArray(item)) {
+//       flatArray = flatArray.concat(flattenArray(item));
+//     } else {
+//       flatArray.push(item);
+//     }
+//   });
+//   return flatArray;
+// }
+// // eslint-disable-next-line func-style
+// function sumFlatArray(array) {
+//   if (array.length > 1) {
+//     return array.pop() + sumFlatArray(array);
+//   } else {
+//     return array[0];
+//   }
+// }
 // eslint-disable-next-line func-style
-function flattenArray(array) {
-  let flatArray = [];
+function sumItems(array) {
+  let result = 0;
   array.forEach(item => {
     if (Array.isArray(item)) {
-      flatArray = flatArray.concat(flattenArray(item));
+      result += sumItems(item);
     } else {
-      flatArray.push(item);
+      result += item;
     }
   });
-  return flatArray;
-}
-// eslint-disable-next-line func-style
-function sumFlatArray(array) {
-  if (array.length > 1) {
-    return array.pop() + sumFlatArray(array);
-  } else {
-    return array[0];
-  }
+  return result;
 }
 
 module.exports = sumItems;
