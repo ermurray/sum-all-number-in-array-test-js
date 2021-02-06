577e8f75e039fd78604755ad3d0dfcd063c12836
ermurray <ermurray31@gmail.com>
2021-02-06 18:01:49 +0000

    recursively flatten and sum array two funtions

--- index.js
+++ index.js
@@ -1,5 +1,28 @@
+// eslint-disable-next-line func-style
 function sumItems(array) {
-  // Sum all the numbers in the array
+  let flatArr = flattenArray(array);
+  let sum = sumFlatArray(flatArr);
+  return sum;
+}
+// eslint-disable-next-line func-style
+function flattenArray(array) {
+  let flatArray = [];
+  array.forEach(item => {
+    if (Array.isArray(item)) {
+      flatArray = flatArray.concat(flattenArray(item));
+    } else {
+      flatArray.push(item);
+    }
+  });
+  return flatArray;
+}
+// eslint-disable-next-line func-style
+function sumFlatArray(array) {
+  if (array.length > 1) {
+    return array.pop() + sumFlatArray(array);
+  } else {
+    return array[0];
+  }
 }
 
 module.exports = sumItems;
